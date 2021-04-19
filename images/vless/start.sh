7da90de286c7840c277afb65ac1edc6bc7bdb51e
ChenTanyi <chentanyi@gmail.com>
2021-04-19 23:40:25 +0800

    feat: add vless

---
+++ images/vless/start.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -e
+
+mkdir /v2ray
+wget https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -P /v2ray
+unzip /v2ray/v2ray-linux-64.zip -d /v2ray
+chmod +x /v2ray/v2ray && mv /v2ray/v2ray /usr/bin
+chmod +x /v2ray/v2ctl && mv /v2ray/v2ctl /usr/bin
+rm -rf /v2ray
+
+cat << EOF > /config.json
+{
+  "inbounds": [
+    {
+      "port": 80,
+      "listen": "0.0.0.0",
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID",
+            "level": 0
+          }
+        ],
+        "decryption": "none",
+        "fallbacks": [
+          {
+            "dest": "httpbin.org:80"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "tcp"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+v2ray -c /config.json
