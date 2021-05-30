acc245c9e23e5842ba8be70e4be793f1ed6009ac
ChenTanyi <chentanyi@gmail.com>
2021-05-30 15:26:00 +0800

    feat: add trailing slash in setup

--- images/v2proxy/setup-v2ray.sh
+++ images/v2proxy/setup-v2ray.sh
@@ -9,10 +9,12 @@ cd /
 rm -rf /tmp/v2ray
 
 export PROTOCOL=${PROTOCOL:-vless}
+export REQ_PATH="${REQ_PATH%/}/"
 
 ALL_ENV='$PORT $REQ_PATH $UUID $PROTOCOL'
 envsubst "$ALL_ENV" < /usr/local/etc/v2ray/config.json.template > /usr/local/etc/v2ray/config.json
 envsubst "$ALL_ENV" < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+sed -i "s/env:PORT/8000/g" /usr/local/etc/v2ray/config.json
 
 mkdir -p /var/log/v2ray/
 /usr/local/bin/v2ray -test -c /usr/local/etc/v2ray/config.json
